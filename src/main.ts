24aeeba561bbf1f60dfe806cc3287925fc00cfdf
Maksym Bondarenko <m.bondarenko@upswot.com>
2024-09-28 14:56:58 +0200

    in process

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createI18n } from "vue-i18n";
 
-createApp(App).use(router).mount("#app");
+import en from "./locales/en.json";
+import uk from "./locales/uk.json";
+
+const i18n = createI18n({
+  locale: "uk",
+  fallbackLocale: "en",
+  messages: {
+    en,
+    uk,
+  },
+});
+
+createApp(App).use(router).use(i18n).mount("#app");
