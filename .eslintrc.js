6b6f64bff67fcd726d09a42eca374abba8cd6f72
Maksym Bondarenko <m.bondarenko@upswot.com>
2024-09-29 19:05:05 +0200

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,28 +8,16 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
     sourceType: 'module',
-    extraFileExtensions: ['.vue'],
   },
   plugins: ['vue', '@typescript-eslint', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended', // Use 'plugin:vue/recommended' if you're using Vue 2
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   rules: {
     'prettier/prettier': 'error',
-    // Corrected rule configuration
-    // 'vue/max-attributes-per-line': [
-    //   'error',
-    //   {
-    //     singleline: 5,
-    //     multiline: {
-    //       max: 1,
-    //       allowFirstLine: false,
-    //     },
-    //   },
-    // ],
-    'vue/singleline-html-element-content-newline': 'off',
-    // Adjust this rule as per your preference
-    'vue/html-self-closing': 'off',
-    // Other rules
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Add any custom rules here
   },
 };
