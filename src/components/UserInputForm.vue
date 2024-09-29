028298c310a66188fbddd29506909ce3332aae19
Maksym Bondarenko <m.bondarenko@upswot.com>
2024-09-29 16:22:50 +0200

    added validation

--- src/components/UserInputForm.vue
+++ src/components/UserInputForm.vue
@@ -3,59 +3,59 @@
     <div>
       <label>{{ $t('currentWeight') }} (kg):</label>
       <input v-model.number="formData.weight" type="number" required />
+      <small v-if="validationErrors.weight" class="error">{{ validationErrors.weight }}</small>
     </div>
     <div>
       <label>{{ $t('targetWeight') }} (kg):</label>
       <input v-model.number="formData.targetWeight" type="number" required />
+      <small v-if="validationErrors.targetWeight" class="error">{{ validationErrors.targetWeight }}</small>
     </div>
     <div>
       <label>{{ $t('timeFrame') }} (days):</label>
       <input v-model.number="formData.timeFrame" type="number" required />
+      <small v-if="validationErrors.timeFrame" class="error">{{ validationErrors.timeFrame }}</small>
     </div>
     <div>
       <label>{{ $t('calorieIntake') }} (kcal):</label>
       <input v-model.number="formData.currentCalorieIntake" type="number" required />
+      <small v-if="validationErrors.currentCalorieIntake" class="error">
+        {{ validationErrors.currentCalorieIntake }}
+      </small>
     </div>
     <div>
       <label>{{ $t('height') }} (cm):</label>
       <input v-model.number="formData.height" type="number" required />
+      <small v-if="validationErrors.height" class="error">{{ validationErrors.height }}</small>
     </div>
     <div>
-      <label>Age (years):</label>
+      <label>{{ $t('age') }} (years):</label>
       <input v-model.number="formData.age" type="number" required />
+      <small v-if="validationErrors.age" class="error">{{ validationErrors.age }}</small>
     </div>
     <div>
-      <label>Gender:</label>
+      <label>{{ $t('gender') }}:</label>
       <select v-model="formData.gender" required>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
+        <option value="male">{{ $t('male') }}</option>
+        <option value="female">{{ $t('female') }}</option>
       </select>
     </div>
     <div>
       <label>{{ $t('activityLevel') }}:</label>
       <select v-model="formData.activityLevel" required>
-        <option value="sedentary">
-          {{ $t('Sedentary (little or no exercise)') }}
-        </option>
-        <option value="light">
-          {{ $t('Lightly active (1-3 days/week)') }}
-        </option>
-        <option value="moderate">
-          {{ $t('Moderately active (3-5 days/week)') }}
-        </option>
-        <option value="active">{{ $t('Very active (6-7 days/week)') }}</option>
-        <option value="veryActive">
-          {{ $t('Extra active (very hard exercise & physical job)') }}
-        </option>
+        <option value="sedentary">{{ $t('sedentary') }}</option>
+        <option value="light">{{ $t('light') }}</option>
+        <option value="moderate">{{ $t('moderate') }}</option>
+        <option value="active">{{ $t('active') }}</option>
+        <option value="veryActive">{{ $t('veryActive') }}</option>
       </select>
     </div>
-    <button type="submit">{{ $t('calculate') }}</button>
+    <button type="submit" :disabled="!isFormValid">{{ $t('calculate') }}</button>
   </form>
 </template>
 
 <script lang="ts">
 import { UserData } from '@/interfaces/UserData';
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, ref, watch, computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
@@ -71,25 +71,146 @@ export default defineComponent({
     const { t } = useI18n();
 
     const formData = ref<UserData>({} as UserData);
+    const validationErrors = ref<Record<string, string>>({});
+    const isFormValid = computed(() => {
+      return Object.keys(validationErrors.value).length === 0;
+    });
 
-    // Watch for changes in initialUserData and update formData
     watch(
       () => props.initialUserData,
       (newVal) => {
         if (newVal) {
           formData.value = { ...newVal };
+          validateForm();
         }
       },
       { immediate: true }
     );
 
+    // Watchers for each field
+    watch(
+      () => formData.value.weight,
+      () => {
+        validateWeight();
+        validateWeightDifference();
+      }
+    );
+
+    watch(
+      () => formData.value.targetWeight,
+      () => {
+        validateTargetWeight();
+        validateWeightDifference();
+      }
+    );
+
+    watch(
+      () => formData.value.timeFrame,
+      () => {
+        validateTimeFrame();
+      }
+    );
+
+    watch(
+      () => formData.value.currentCalorieIntake,
+      () => {
+        validateCurrentCalorieIntake();
+      }
+    );
+
+    watch(
+      () => formData.value.height,
+      () => {
+        validateHeight();
+      }
+    );
+
+    watch(
+      () => formData.value.age,
+      () => {
+        validateAge();
+      }
+    );
+
+    const validateForm = () => {
+      validateWeight();
+      validateTargetWeight();
+      validateWeightDifference();
+      validateTimeFrame();
+      validateCurrentCalorieIntake();
+      validateHeight();
+      validateAge();
+    };
+
+    const validateWeight = () => {
+      if (formData.value.weight < 40 || formData.value.weight > 250) {
+        validationErrors.value.weight = t('invalidWeight');
+      } else {
+        delete validationErrors.value.weight;
+      }
+    };
+
+    const validateTargetWeight = () => {
+      if (formData.value.targetWeight < 40 || formData.value.targetWeight > 250) {
+        validationErrors.value.targetWeight = t('invalidTargetWeight');
+      } else {
+        delete validationErrors.value.targetWeight;
+      }
+    };
+
+    const validateWeightDifference = () => {
+      const weightDiff = Math.abs(formData.value.weight - formData.value.targetWeight);
+      if (weightDiff > 80) {
+        validationErrors.value.weightDifference = t('invalidWeightDifference');
+      } else {
+        delete validationErrors.value.weightDifference;
+      }
+    };
+
+    const validateTimeFrame = () => {
+      if (formData.value.timeFrame < 14 || formData.value.timeFrame > 365) {
+        validationErrors.value.timeFrame = t('invalidTimeFrame');
+      } else {
+        delete validationErrors.value.timeFrame;
+      }
+    };
+
+    const validateCurrentCalorieIntake = () => {
+      if (formData.value.currentCalorieIntake < 800 || formData.value.currentCalorieIntake > 8000) {
+        validationErrors.value.currentCalorieIntake = t('invalidCalorieIntake');
+      } else {
+        delete validationErrors.value.currentCalorieIntake;
+      }
+    };
+
+    const validateHeight = () => {
+      if (formData.value.height < 100 || formData.value.height > 250) {
+        validationErrors.value.height = t('invalidHeight');
+      } else {
+        delete validationErrors.value.height;
+      }
+    };
+
+    const validateAge = () => {
+      if (formData.value.age < 10 || formData.value.age > 120) {
+        validationErrors.value.age = t('invalidAge');
+      } else {
+        delete validationErrors.value.age;
+      }
+    };
+
     const onSubmit = () => {
-      emit('calculate', { ...formData.value });
+      validateForm();
+      if (isFormValid.value) {
+        emit('calculate', { ...formData.value });
+      }
     };
 
     return {
       t,
       formData,
+      validationErrors,
+      isFormValid,
       onSubmit,
     };
   },
@@ -108,5 +229,10 @@ label {
 input,
 select {
   width: 100%;
+}
+
+.error {
+  color: red;
+  font-size: 0.9em;
 }
 </style>
