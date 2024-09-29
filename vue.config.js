03c215c15996d281db65e60033214ccf8c050abb
Maksym Bondarenko <m.bondarenko@upswot.com>
2024-09-29 23:29:41 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from '@vue/cli-service';
-export default defineConfig({
-  transpileDependencies: true,
-});
-
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: '/weight-prediction-app',
 });
