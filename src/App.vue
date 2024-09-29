f79653f69ab71d9c0686ea0d950fb676ad632f7b
Maksym Bondarenko <m.bondarenko@upswot.com>
2024-09-29 11:35:10 +0200

    added language switcher

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <router-view />
-  <nav>
-    <router-link to="/">Main</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div id="app">
+    <div class="language-switcher">
+      <button @click="changeLanguage('uk')">Українська</button>
+      <button @click="changeLanguage('en')">English</button>
+    </div>
+    <router-view />
+    <nav>
+      <router-link to="/">{{ t('main') }}</router-link> |
+      <router-link to="/about">{{ t('about') }}</router-link>
+    </nav>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const { locale, t } = useI18n();
+
+    const changeLanguage = (lang: string) => {
+      locale.value = lang;
+    };
+
+    return {
+      changeLanguage,
+      t,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -13,6 +40,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.language-switcher {
+  margin: 20px;
 }
 
 nav {
